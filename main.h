1c29b92ba0ccd74ac0c759cd455d8b2f05bb9605
Mark Collins <136537528+iamarkc@users.noreply.github.com>
2023-08-21 16:22:25 +0300

    blank 2

--- main.h
+++ main.h
@@ -1,10 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdarg.h>
 
-int _printf(const char *format, ...);
-int print_char(va_list types, char buffer[],
-int print_string(va_list types, char buffer[],
-int print_percent(va_list types, char buffer[],
-
-#endif
