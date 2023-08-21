f5a5f889486b98a65863dcb574a71c02ec56348a
Nomee07 <gigabanomfundo@gmail.com>
2023-08-21 08:40:08 -0700

    printf.c

--- main.h
+++ main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
+void print_char(char c, int *count)
+void print_string(char *s, int *count)i
 int _putchar(char c);
 
 #endif
